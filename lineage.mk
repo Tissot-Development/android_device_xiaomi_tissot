2a3c840a25aef22fb580d6865f7c2eb760773a5a
ghpranav <bedrepranav@gmail.com>
2018-02-12 20:54:03 +0530

    Tissot: Initial Oreo bringup

--- lineage.mk
+++ lineage.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, device/xiaomi/tissot/full_tissot.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # A/B updater
 AB_OTA_UPDATER := true
