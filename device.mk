39bf6b776133b8b433305cb8bfd6116f87092eb7
dianlujitao <dianlujitao@lineageos.org>
2018-02-20 08:47:00 -0600

    tissot: Build LiveDisplay HAL

--- device.mk
+++ device.mk
@@ -284,6 +284,14 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     lights.msm8953
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm \
+    vendor.lineage.livedisplay-V1.0-java
+
+PRODUCT_BOOT_JARS += \
+    vendor.lineage.livedisplay-V1.0-java
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
