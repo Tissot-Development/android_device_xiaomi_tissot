9f7cdbe116799e2d5afb5778cd32839383c82909
ghpranav <bedrepranav@gmail.com>
2018-02-20 11:05:58 +0530

    Tissot: Use libselinux

--- init/Android.mk
+++ init/Android.mk
@@ -23,6 +23,8 @@ LOCAL_C_INCLUDES := system/core/init
 LOCAL_MODULE := libinit_tissot
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := init_tissot.cpp
-LOCAL_STATIC_LIBRARIES := libbase
+LOCAL_STATIC_LIBRARIES := \
+      libbase \
+      libselinux
 
 include $(BUILD_STATIC_LIBRARY)
