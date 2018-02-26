d1fa99fed595e1df7f0704f629f3e1d580181291
ghpranav <bedrepranav@gmail.com>
2018-02-26 15:35:54 +0530

    Tissot: Build 32-bit android.frameworks.displayservice@1.0

--- device.mk
+++ device.mk
@@ -125,6 +125,7 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0_32 \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
