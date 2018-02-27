71b73b94e52cabf7e6c7bac482e3adade65b48ca
kunal <kunalshah912@gmail.com>
2018-02-27 21:27:31 +0000

    fix WIFI_DRIVER_MODULE_PATH

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -232,7 +232,7 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_PATH := "/system/vendor/lib/modules/wlan.ko"
 WIFI_DRIVER_MODULE_NAME := "wlan"
 
 # Inherit from the proprietary version
