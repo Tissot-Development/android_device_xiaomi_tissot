1dd54d7e49c9fa24a2cf6b571c55de2f038cb049
ghpranav <bedrepranav@gmail.com>
2018-03-08 17:35:44 +0530

    Tissot: Changes for camera

--- device.mk
+++ device.mk
@@ -133,6 +133,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.device@3.3-impl \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
