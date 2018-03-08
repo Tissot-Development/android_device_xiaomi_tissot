50e93dd2177ffbe9a7d1c23baedce4cb620c48cb
ghpranav <bedrepranav@gmail.com>
2018-03-08 23:31:16 +0530

    Tissot: Build libmmcamera_interface

--- device.mk
+++ device.mk
@@ -132,6 +132,8 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     camera.device@3.3-impl \
+    libmmcamera_interface \
+    libmmcamera_interface2 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
