dcb40529343e0778a460acf2d271b66594d4c1ea
ghpranav <bedrepranav@gmail.com>
2018-03-10 04:26:29 +0000

    Tissot: Try to fix cam

--- device.mk
+++ device.mk
@@ -125,19 +125,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     MiuiCamera \
     libshims_skia \
-    Snap
+    QCamera2
 
-PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0_32 \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    camera.device@3.3-impl \
-    libmmcamera_interface \
-    libmmcamera_interface2 \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Configstore
 PRODUCT_PACKAGES += \
