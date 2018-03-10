a1cdc47ffd8fbecdf1d34b126031dd4f2f7129ee
Flex911 <dedsa2002@gmail.com>
2018-03-10 18:28:37 +0300

    Tissot: Configure dm-verity

--- device.mk
+++ device.mk
@@ -432,3 +432,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0
+
+# Verity
+PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/7824900.sdhci/by-name/system
+$(call inherit-product, build/target/product/verity.mk)
