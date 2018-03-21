5d6e601f2df45f54d3921f265a85e88b53d9b57a
Pranav Bedre <bedrepranav@gmail.com>
2018-03-21 19:15:18 +0530

    Tissot: We are official now

--- rr.mk
+++ rr.mk
@@ -60,6 +60,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys" \
     PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
-
-# Unofficial build ID
-TARGET_UNOFFICIAL_BUILD_ID := TEAMOREO
