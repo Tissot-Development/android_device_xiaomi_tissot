ae65629f7504b3fc25070e9faa93cd1b2d15c8dc
Pranav Bedre <bedrepranav@gmail.com>
2018-03-21 20:38:34 +0530

    Tissot: Add backlight path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,6 +117,7 @@ TARGET_TS_MAKEUP := true
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # CNE / DPM
 BOARD_USES_QCNE := true
