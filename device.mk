af2d1a0527f4f764d88f0646c7b637c4dcbd8fa5
ghpranav <bedrepranav@gmail.com>
2018-04-11 07:00:37 -0400

    tissot: Add powerhint from stock

--- device.mk
+++ device.mk
@@ -326,6 +326,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.1-impl \
     android.hardware.power@1.1-service-qti
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
