84c4e954b89cfd3d555f839915bb76565f8a5df7
TheScarastic <warabhishek@gmail.com>
2018-05-22 14:31:28 +0530

    tissot: Generate thermal-engine.conf
     * adb shell thermal-engine –o > thermal-engine.conf
    
    Change-Id: If934f33d43c7d74008e63ceef8dd03d420c624dc

--- device.mk
+++ device.mk
@@ -389,6 +389,9 @@ PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
 # Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
