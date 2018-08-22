c99ef375a79b12cefc730b0ae914d7cd8c0e65b4
Mountaser Halak <mountaser1998@gmail.com>
2018-08-22 15:32:47 +0200

    tissot: Nuke framework-res
    Dont look at me go blame the RRO team xD

--- device.mk
+++ device.mk
@@ -23,9 +23,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
-
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
