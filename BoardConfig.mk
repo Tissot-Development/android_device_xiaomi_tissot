b6e61b7406aebd4ada1b591b0993fbca60bd3a0f
mountaser <mountaser1998@gmail.com>
2019-01-03 17:34:33 +0100

    Revert "tissot: Ignore Sepolicy for now"
    This reverts commit 6b9238a83299c09aedac477d56186f0765e2c777.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,7 @@ TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 VENDOR_SECURITY_PATCH := 2018-12-05
 
 # Sepolicy
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/tissot/BoardConfigVendor.mk
