52bcf7d015359ddeaf047850a0c0143870d3be7a
Artem Borisov <dedsa2002@gmail.com>
2019-04-27 16:11:22 +0530

    tissot: Add fluence properties
    * Fluence should be disabled as per stock
    
    Change-Id: Ife676b70d03d6c17395ddbf4db3ff8c54e726573

--- prop.mk
+++ prop.mk
@@ -1,3 +1,10 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.audio.fluence.speaker=true \
+persist.vendor.audio.fluence.voicecall=true \
+persist.vendor.audio.fluence.voicerec=false \
+ro.vendor.audio.sdk.fluencetype=none
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.camera.eis.enable=1 \
